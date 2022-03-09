2e7a4b35f3efd1ac6fae685d936af69d32ed97d2
Brandon Van Blarcum <41463636+Drofal980@users.noreply.github.com>
2022-03-09 10:33:01 -0700

    Two snakes are drawn and move independently

--- cycle/game/scripting/control_actors_action.py
+++ cycle/game/scripting/control_actors_action.py
@@ -2,7 +2,6 @@ import constants
 from game.scripting.action import Action
 from game.shared.point import Point
 
-#Added later by teacher, originally found on lines 38-51
 LEFT = Point(-constants.CELL_SIZE, 0)
 RIGHT = Point(constants.CELL_SIZE, 0)
 UP = Point(0, -constants.CELL_SIZE)
@@ -25,7 +24,8 @@ class ControlActorsAction(Action):
             keyboard_service (KeyboardService): An instance of KeyboardService.
         """
         self._keyboard_service = keyboard_service
-        self._direction = RIGHT #Point(constants.CELL_SIZE, 0)
+        self._direction_player_1 = RIGHT
+        self._direction_player_2 = RIGHT
 
     def execute(self, cast, script):
         """Executes the control actors action.
@@ -34,27 +34,47 @@ class ControlActorsAction(Action):
             cast (Cast): The cast of Actors in the game.
             script (Script): The script of Actions in the game.
         """
+
+        players = cast.get_actors("snakes")
+
         # left
-        if self._keyboard_service.is_key_down('a') and self._direction != RIGHT: #After 'and' was added by teacher
-            self._direction = LEFT #Point(-constants.CELL_SIZE, 0)
+        if self._keyboard_service.is_key_down('a') and self._direction_player_1 != RIGHT: 
+            self._direction_player_1 = LEFT
+
+        if self._keyboard_service.is_key_down('j') and self._direction_player_2 != RIGHT: 
+            self._direction_player_2 = LEFT
         
         # right
-        if self._keyboard_service.is_key_down('d') and self._direction != LEFT:
-            self._direction = RIGHT #Point(constants.CELL_SIZE, 0)
+        if self._keyboard_service.is_key_down('d') and self._direction_player_1 != LEFT:
+            self._direction_player_1 = RIGHT
+
+        if self._keyboard_service.is_key_down('l') and self._direction_player_2 != LEFT: 
+            self._direction_player_2 = RIGHT
         
         # up
-        if self._keyboard_service.is_key_down('w') and self._direction != DOWN:
-            self._direction = UP #Point(0, -constants.CELL_SIZE)
+        if self._keyboard_service.is_key_down('w') and self._direction_player_1 != DOWN:
+            self._direction_player_1 = UP
+        
+        if self._keyboard_service.is_key_down('i') and self._direction_player_2 != DOWN:
+            self._direction_player_2 = UP
         
         # down
-        if self._keyboard_service.is_key_down('s') and self._direction != UP:
-            self._direction = DOWN #Point(0, constants.CELL_SIZE)
-        
-        snake = cast.get_first_actor("snakes")
-        head = snake.get_segments()[0]
-        opposite_head_velocity = head.get_velocity().reverse()
+        if self._keyboard_service.is_key_down('s') and self._direction_player_1 != UP:
+            self._direction_player_1 = DOWN
 
-        if self._direction.equals(opposite_head_velocity) == False: #I added To fix bug where snake turns 180 degrees, which I think works better
-            snake.turn_head(self._direction)
+        if self._keyboard_service.is_key_down('k') and self._direction_player_2 != UP:
+            self._direction_player_2 = DOWN
+        
+        player_1_head = players[0].get_segments()[0]
+        opposite_head_velocity = player_1_head.get_velocity().reverse()
+
+        if self._direction_player_1.equals(opposite_head_velocity) == False: #I added To fix bug where snake turns 180 degrees, which I think works better
+            players[0].turn_head(self._direction_player_1)
+
+        player_2_head = players[1].get_segments()[0]
+        opposite_head_velocity = player_2_head.get_velocity().reverse()
+
+        if self._direction_player_2.equals(opposite_head_velocity) == False: #I added To fix bug where snake turns 180 degrees, which I think works better
+            players[1].turn_head(self._direction_player_2)
 
         
--- cycle/game/scripting/draw_actors_action.py
+++ cycle/game/scripting/draw_actors_action.py
@@ -28,13 +28,15 @@ class DrawActorsAction(Action):
         """
         score = cast.get_first_actor("scores")
         food = cast.get_first_actor("foods")
-        snake = cast.get_first_actor("snakes")
-        segments = snake.get_segments()
+        snakes = cast.get_actors("snakes")
+        player_1_segments = snakes[0].get_segments()
+        player_2_segments = snakes[1].get_segments()
         messages = cast.get_actors("messages")
 
         self._video_service.clear_buffer()
         self._video_service.draw_actor(food)
-        self._video_service.draw_actors(segments)
+        self._video_service.draw_actors(player_1_segments)
+        self._video_service.draw_actors(player_2_segments)
         self._video_service.draw_actor(score)
         self._video_service.draw_actors(messages, True)
         self._video_service.flush_buffer()
